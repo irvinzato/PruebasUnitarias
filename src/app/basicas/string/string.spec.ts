62f1d68a59b9781138a814dccfa01f61800e1d21
irvinzato <multizato@hotmail.com>
2022-07-22 20:37:02 -0500

    Creé segunda prueba con .toContain

--- src/app/basicas/string/string.spec.ts
+++ src/app/basicas/string/string.spec.ts
@@ -1,13 +1,24 @@
-//Sirve para agrupar pruebas
+//describe - Sirve para agrupar pruebas
 //describe('Pruebas de Strings');
-//Sirve para decir que es una prueba en especifico
+//it -Sirve para decir que es una prueba en especifico
 //it('Debe de regresar un String');
 import { mensaje } from "./string";
 
 describe( 'Pruebas de Strings', () =>  {
+    //Primer prueba
     it( 'Debe de regresar un string', () => {
         const respuesta = mensaje('Irving');
 
-        expect(  typeof respuesta ).toBe('string');
+        //Usar el ".toBe" es como decirle que "typeof respuesta === string"
+        expect( typeof respuesta ).toBe('string');
+    });
+
+    //Segunda prueba
+    it( 'Debe de retornar un saludo con el nombre enviado', () => {
+        const nombre = 'Irving';
+        const respuesta = mensaje( nombre );
+
+        //Cambie ".toBe" a ".toContain" para decirle que no sea igual, solo que contenga
+        expect( respuesta ).toContain( nombre );
     });
 });
--- src/app/basicas/string/string.ts
+++ src/app/basicas/string/string.ts
@@ -1,5 +1,5 @@
-
+//Debo poner export para poder usar la función fuera del archivo u hacer la prueba
 export function mensaje( nombre: string ): string {
     return `Saludos ${ nombre }`;
-    /* return false; */
+    /* return false; Asi hago que falle al proposito y veo que me muestra el "ng test" o Jazmine */
 }
