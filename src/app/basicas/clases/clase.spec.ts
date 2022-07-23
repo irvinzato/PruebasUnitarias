c9ce1381e5e3e31ae78f83e0e8ea4bc5ddf7fcf2
irvinzato <multizato@hotmail.com>
2022-07-22 23:18:23 -0500

    Ciclos de pruebas, beforeAll, beforeEach, afterAll, afterEach

--- src/app/basicas/clases/clase.spec.ts
+++ src/app/basicas/clases/clase.spec.ts
@@ -4,13 +4,32 @@ describe('Pruebas de clase Jugador', () => {
    /*De esta manera no duplico su iniciación pero se acumulan las evaluaciones(Por eso falla la segunda prueba) 
     En la prueba 1 queda el hp en 80, mas 50 que le hago en la segunda prueba, quedaria en 30
    Aqui es donde entran lo siguientes ciclos de las pruebas
-    beforeAll();
-    beforeEach();
+    beforeAll(); - Antes de que se ejecuten todas las pruebas
+    beforeEach(); - Antes de que se ejecute cada prueba
 
-    afterAll();
-    afterEach();
+    afterAll(); - Despues de que todas las pruebas finalizan
+    afterEach(); - Cada vez que termina una prueba
    */
-    const jugador = new Jugador();
+    let jugador = new Jugador();
+
+    beforeAll( () => {
+        console.log("Before All");
+    }); 
+
+    beforeEach( () => {
+        console.log("Before Each");
+        //jugador.hp = 100;
+        //La linea de arriba funciona pero solo para una variable, con la siguiente linea restablezco todo el objeto y cada una de sus propiedades
+        jugador = new Jugador();
+    }); 
+
+    afterAll( () => {
+        console.log("After All");
+    });
+
+    afterEach( () => {
+        console.log("After Each");
+    }); 
 
     it('Debe retornar 80 de HP si recibe 20 de daño', () => {
         //const jugador = new Jugador();
@@ -23,7 +42,7 @@ describe('Pruebas de clase Jugador', () => {
         //const jugador = new Jugador();
         const resp = jugador.recibeDanio(50);
 
-        expect( resp ).toBe(50); //30 seria el resultado de esta manera
+        expect( resp ).toBe(50);
     });
 
 });
